58d9c18339653fb29734693b048428240602a023
bxi64u <bxi64u@SZA0030883>
2023-03-14 09:02:42 +0100

    letrehozas commit

--- Excel_NegyedikHet/Excel_NegyedikHet/Form1.cs
+++ Excel_NegyedikHet/Excel_NegyedikHet/Form1.cs
@@ -16,17 +16,48 @@ namespace Excel_NegyedikHet
     {
         List<Flat> _flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            //CreateExcel();
         }
 
         void LoadData()
         {
             _flats = context.Flats.ToList();
 
+        }
+
+        void CreatExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                //CrateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+
+            }
         }
     }
 }
