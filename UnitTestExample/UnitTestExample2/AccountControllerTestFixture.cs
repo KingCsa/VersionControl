a38bb7f3c0edbaa6e14f3b97134350555523ac2c
bxi64u <bxi64u@SZA0030883>
2023-05-02 09:28:04 +0200

    befejezés

--- UnitTestExample/UnitTestExample2/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample2/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,11 @@ namespace UnitTestExample2
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [Test,
+         TestCase("abcd1234", false),
+         TestCase("irf@uni-corvinus", false),
+         TestCase("irf.uni-corvinus.hu", false),
+         TestCase("irf@uni-corvinus.hu", true)]
 
         public void TestValidateEmail(string email, bool expectedResult)
         {
@@ -19,6 +24,71 @@ namespace UnitTestExample2
             var actualResult = accountController.ValidateEmail(email);
 
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test,
+         TestCase("ABCDabcd", false),
+         TestCase("ABCD1234", false),
+         TestCase("abcd1234", false),
+         TestCase("Abcd12", false),
+         TestCase("Abcd1234", true)]
+
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [
+          Test,
+          TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+          TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
+
+
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.Register(email, password);
+
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus", "Abcd1234"),
+            TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+            TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+            TestCase("irf@uni-corvinus.hu", "Ab1234"),
+        ]
+
+
+        public void TestRegisterValidateException(string email, string password)
+        {
+            var accountController = new AccountController();
+
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
         }
     }
 }
