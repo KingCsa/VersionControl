f213218512515a2f8bc4252c3a0e77526528dd52
bxi64u <bxi64u@SZA0030883>
2023-05-09 08:37:52 +0200

    fresh

--- Mikrosimulation/Mikrosimulation/Form1.cs
+++ Mikrosimulation/Mikrosimulation/Form1.cs
@@ -13,13 +13,13 @@ namespace Mikrosimulation
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
         public Form1()
         {
             InitializeComponent();
-
-            Gender g = Gender.Male;
-
-
         }
     }
 }
