e4628b64ac3b46f9192ff2971d6e6acf4ecd7288
bxi64u <bxi64u@SZA0030883>
2023-05-09 08:30:37 +0200

    refresh

--- Mikrosimulation/Mikrosimulation/Form1.cs
+++ Mikrosimulation/Mikrosimulation/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mikrosimulation.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,10 @@ namespace Mikrosimulation
         public Form1()
         {
             InitializeComponent();
+
+            Gender g = Gender.Male;
+
+
         }
     }
 }
