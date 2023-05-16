87d371ee66a3da8d82057d0009795527ff8e12e3
bxi64u <bxi64u@SZA0030883>
2023-05-16 09:36:34 +0200

    plus and test

---
+++ MintaZH3/MintaZH3/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4aa93f8-1bd8-4831-ad5f-b974de06d0a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MintaZH3/MintaZH3/UnitTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using MintaZH3.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+
+    public class ChildTestFixture
+    {
+        [
+            Test,
+            TestCase(1, true),
+            TestCase(2, true),
+            TestCase(3, true),
+            TestCase(4, true),
+            TestCase(5, true),
+            TestCase(0, false),
+            TestCase(666, false)
+        ]
+        public void TestCheckBehaviour(int value, bool expectedResult)
+        {
+            var c = new Child();
+
+            var actualResult = c.CheckBehaviour(value);
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+    }
+}
