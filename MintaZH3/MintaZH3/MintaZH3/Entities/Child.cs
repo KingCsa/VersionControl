3fb237c8de5dd00837a81fafb24a551ce50858d7
bxi64u <bxi64u@SZA0030883>
2023-05-16 08:27:04 +0200

    child

---
+++ MintaZH3/MintaZH3/MintaZH3/Entities/Child.cs
@@ -0,0 +1,25 @@
+﻿using MintaZH3.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH3.Entities
+{
+    public class Child
+    {
+        public string Name { get; set; }
+        public Behaviour YearlyBehaviour { get; set; }
+
+        public bool CheckBehaviour(int value)
+        {
+            if (value >= 1 && value <= 5)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
